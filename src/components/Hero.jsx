0c57dbb4c089b113d96d8877c0695ff2638fab98
bryanmsombra27 <bryanjm96@gmail.com>
2024-12-26 12:29:56 -0600

    updating  portafolio part-1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
     return (
 
         <section className=" h-full   mt-9 flex flex-col items-center space-y-9 mb-10   md:h-[600px] md:mb-0">
-            <h1 className="text-center text-5xl font-bold mt-12">{english ? text.title : "Desarrollador Frontend"}</h1>
-            <h3 className="text-center text-2xl mt-9">{english ? text.description : "Desarrollo la maquetación web y realizo la funcionalidad visual de la aplicación"
+            <h1 className="text-center text-5xl font-bold mt-12">{english ? text.title : "Desarrollador FullStack"}</h1>
+            <h3 className="text-center text-2xl mt-9 w-[900px]">{english ? text.description : "Creo API's para la comunicación entre el backend y frontend y  Desarrollo aplicaciones web  o moviles segun las necesidades del proyecto."
             }</h3>
             <div className="bg-cyan-700 rounded-full">
                 <img src="/img/avatar.svg" alt="avatar" className="w-80 pt-3  border-cyan-400 border-8 rounded-full" />
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,19 +2,19 @@ import { useContext } from "react";
 import { ShadowContext } from "../context/shadowContext";
 const Welcome = () => {
 
-    const {heroText,english} = useContext(ShadowContext);
+    const { heroText, english } = useContext(ShadowContext);
 
     return (
 
         <section className="h-full  bg-cyan-500 space-y-[50px]   md:h-[400px] "  >
             <div className="md:w-[1200px] h-full flex   flex-col justify-evenly items-center mx-auto  ">
                 <div className="text-center  mt-[50px] md:mt-0 ">
-                    <h3 className="font-bold text-4xl text-center  p-3 md:p-0 ">{english ? heroText.welcome:"Hola! Soy Bryan Ochoa. Te doy la  Bienvenida a mi Portafolio Web"  } </h3>
+                    <h3 className="font-bold text-4xl text-center  p-3 md:p-0 ">{english ? heroText.welcome : "Hola! Soy Bryan Ochoa. Te doy la  Bienvenida a mi Portafolio Web"} </h3>
 
                 </div>
                 <div>
                     <p className="text-2xl text-justify p-9 md:p-0">
-                        {english ? heroText.description: "Desde que comence mi viaje en el desarrollo web hace 4 años he desarrollado proyectos personales y recientemente egrese de la universidad, comenzando asi la busqueda para  conseguir un empleo como desarrollador frontend. A continuacion, puedes hechar un ojo sobre los trabajos en los que he colaborado" }
+                        {english ? heroText.description : "Desde que comence mi viaje en el desarrollo web hace 4 años he desarrollado proyectos personales y 2 años trabajando formalmente para empresas desarrollando aplicaciones web y moviles.A continuacion, puedes examinar los trabajos que he realizado."}
                     </p>
                 </div>
 
